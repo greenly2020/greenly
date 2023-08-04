d9f62ec12663bf4620d0d219c6696d8a1a107788
Vitaliy Hula <gvnhak@gmail.com>
2023-08-04 18:25:44 +0300

    Setup auto deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dev.backend.greenly.co',
+      script: 'npm',
+      args: 'start',
+      cwd: './backend',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 1337,
+      },
+    },
+    {
+      name: 'dev.frontend.greenly.co',
+      script: 'npm',
+      args: 'start',
+      cwd: './frontend',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000,
+      },
+    },
+  ],
+};
