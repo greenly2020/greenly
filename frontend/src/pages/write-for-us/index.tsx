51cbd7a003d5be8fea99bd9c1c9fab6666a1f56c
yuramol <yuramol86@gmail.com>
2023-08-10 16:45:08 +0300

    Copy editing

--- frontend/src/pages/write-for-us/index.tsx
+++ frontend/src/pages/write-for-us/index.tsx
@@ -9,7 +9,7 @@ const WriteForUs = () => {
       <Head>
         <title>Write for us</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content='Write for us' />
+        <meta name="description" content="Write for us" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainLayout>
