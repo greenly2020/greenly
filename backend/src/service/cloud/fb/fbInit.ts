aa7951c348d09c50dacb5b7eb6e8376d3b28dcae
Vitaliy Hula <gvnhak@gmail.com>
2023-09-07 11:16:17 +0300

    Change fb credentials

--- backend/src/service/cloud/fb/fbInit.ts
+++ backend/src/service/cloud/fb/fbInit.ts
@@ -1,6 +1,6 @@
 import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
 
-import * as serviceAccount from '../../../../greenly-9aa89-af594bbda610.json';
+import * as serviceAccount from '../../../../greenly-9aa89-7874a7b2bfd1.json';
 
 initializeApp({
   credential: cert(serviceAccount as ServiceAccount),
