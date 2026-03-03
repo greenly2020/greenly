48a6fae8fd1683e4772b4853baf73731ef16dbaa
Ted Shabecoff <tshabecoff@gmail.com>
2026-03-03 16:33:36 -0500

    Update MailForm.tsx
    Updated typography

--- frontend/src/uiCore/components/MailForm/MailForm.tsx
+++ frontend/src/uiCore/components/MailForm/MailForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export interface IMailFormProps {
     variant?: "primary" | "secondary";
@@ -7,35 +7,40 @@ export interface IMailFormProps {
 
 export const MailForm = React.forwardRef<HTMLElement, IMailFormProps>(
     (props, ref) => {
-          return (
-                  <Box
-                            sx={{
-                                        width: "100%",
-                                        maxWidth: "100%",
-                                        display: "flex",
-                                        justifyContent: "center",
-                            }}
-                          >
-                          <script async src="https://subscribe-forms.beehiiv.com/embed.js"></script>script>
-                          <iframe
-                                      src="https://subscribe-forms.beehiiv.com/889aa328-9f35-4546-ae95-6457bb5765b5"
-                                      className="beehiiv-embed"
-                                      data-test-id="beehiiv-embed"
-                                      frameBorder={0}
-                                      scrolling="no"
-                                      style={{
-                                                    width: "456px",
-                                                    height: "291px",
-                                                    margin: 0,
-                                                    borderRadius: "0px",
-                                                    backgroundColor: "transparent",
-                                                    boxShadow: "none",
-                                                    maxWidth: "100%",
-                                      }}
-                                    />
-                  </Box>Shou
-                );
-    }I 
-  );
+        return (
+            <Box
+                sx={{
+                    width: "100%",
+                    maxWidth: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <Typography variant="h6" sx={{ mb: 2 }}>
+                    Enter your email for our monthly digest
+                </Typography>
+                <script async src="https://subscribe-forms.beehiiv.com/embed.js"></script>
+                <iframe
+                    src="https://subscribe-forms.beehiiv.com/889aa328-9f35-4546-ae95-6457bb5765b5"
+                    className="beehiiv-embed"
+                    data-test-id="beehiiv-embed"
+                    frameBorder={0}
+                    scrolling="no"
+                    style={{
+                        width: "456px",
+                        height: "291px",
+                        margin: 0,
+                        borderRadius: "0px",
+                        backgroundColor: "transparent",
+                        boxShadow: "none",
+                        maxWidth: "100%",
+                    }}
+                />
+            </Box>
+        );
+    }
+);
 
-MailForm.displayName = "MailForm";</Box>
+MailForm.displayName = "MailForm";
